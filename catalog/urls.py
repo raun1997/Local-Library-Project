86dfae50f675c94e5a0e28ffc91ca7f5f78467b4
raun1997 <raunakmitra7@gmail.com>
2024-11-24 22:39:26 +0530

    URL mapping for our app catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path 
+from django.urls import path
 from . import views    # from current directory, import views
 
 urlpatterns = [
-
+    path('', views.index, name="index"),
 ]
