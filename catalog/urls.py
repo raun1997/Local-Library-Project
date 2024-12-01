a8fdde5829561de5880d8e1a906c4f0394d7c712
raun1997 <raunakmitra7@gmail.com>
2024-12-01 21:54:51 +0530

    changed the URL for each book

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,6 @@ from . import views    # from current directory, import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('books/', views.books, name="books"),
-    path('<int:pk>/', views.book_details, name="book_details"),
+    path('book/<int:pk>/', views.book_details, name="book_details"),
     path('authors/', views.authors, name="authors"),
 ]
