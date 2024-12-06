8deb4a0f5f79a5d510efa57bb654290a77b42297
raun1997 <raunakmitra7@gmail.com>
2024-12-06 13:09:23 +0530

    added django-debug-toolbar's URLs to our project's URLconf

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +28,7 @@ urlpatterns = [
     path('catalog/', include('catalog.urls')),
     # add URL maps to redirect the base URL to our main app
     path('', RedirectView.as_view(url="catalog/", permanent=True)),
+    debug_toolbar_urls()
 ]
 
 
